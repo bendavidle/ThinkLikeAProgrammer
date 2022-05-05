a487dc454977eb18784ad1c8fd901c5773be163d
Ben David <Ben David@DESKTOP-UB5EAVM>
2022-05-05 13:09:56 +0200

    Add project files.

---
+++ ThinkLikeAProgrammer/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace ThinkLikeAProgrammer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //twoone(false);
+            //twotwo();
+            //twothree();
+
+            //WordCounter.Start();
+            SubstitutionCypher.Start();
+        }
+
+        private static void twoone(bool inverted)
+        {
+            /*
+                 ########
+                  ######
+                   ####
+                    ##
+                        */
+            int length = 8;
+            int height = 4;
+
+            if (inverted)
+            {
+                length = 2;
+                for (int j = 0; j < height; j++)
+                {
+
+                    drawBlank((8 - length) / 2);
+                    drawHash(length);
+                    newLine();
+                    length += 2;
+                }
+            }
+            else
+            {
+                for (int j = 0; j < height; j++)
+                {
+
+                    drawBlank((8 - length) / 2);
+                    drawHash(length);
+                    newLine();
+                    length -= 2;
+                }
+
+            }
+
+        }
+
+        private static void twotwo()
+        {
+            twoone(true);
+            twoone(false);
+        }
+
+        private static void twothree()
+        {
+            /*
+                #            #
+                 ##        ## 
+                  ###    ###
+                   ########
+                   ########
+                  ###    ###
+                 ##        ##
+                #            # 
+            */
+            int length = 14;
+            int[] spaces = new int[] {0, 1, 2, 3, 3, 2, 1, 0};
+
+            foreach (int space in spaces)
+            {
+                drawBlank(space);
+                drawHash(space + 1);
+                drawBlank(length - 4 * space - 2);
+                drawHash(space + 1);
+                newLine();
+            }
+
+
+        }
+
+        private static void drawHash(int i)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write('#');
+            }
+
+        }
+
+        private static void drawBlank(int i)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(' ');
+            }
+        }
+
+        private static void newLine()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ThinkLikeAProgrammer/WordCounter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkLikeAProgrammer
+{
+    internal class WordCounter
+    {
+        internal static void Start()
+        {
+            Console.Write("Write something: ");
+            string sentence = Console.ReadLine();
+            int words = CountWords(sentence);
+            int vowels = CountVowels(sentence);
+            int longest = LongestWordLength(sentence);
+            Console.WriteLine($"\"{sentence}\" has {words} words, {vowels} vowels and the longest word is {longest} characters long.");
+        }
+
+        private static int CountWords(string text)
+        {
+            string[] words = text.Split(' ');
+            return words.Length;
+        }
+
+        private static int CountVowels(string text)
+        {
+            int count = 0;
+            foreach (char c in text)
+            {
+                if (IsVowel(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private static int LongestWordLength(string text)
+        {
+            string largest = "";
+            string[] words = text.Split(' ');
+            foreach (string word in words)
+            {
+                if (word.Length > largest.Length)
+                {
+                    largest = word;
+                }
+            }
+            return largest.Length;
+        }
+
+        private static bool IsVowel(char c)
+        {
+            char[] vowels = new char[] { 'a', 'e', 'u', 'i', 'o' };
+            return vowels.Contains(c);
+        }
+    }
+
+    
+}
